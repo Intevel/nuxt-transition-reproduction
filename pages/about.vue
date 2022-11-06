44a0c114c4ac808901e71540734efcf7f496d9e4
Conner Bachmann <github@conner-bachmann.de>
2022-11-06 17:01:33 +0100

    feat: try to reproduce bug in stackblitz

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+    <h1>This is my About page</h1>
+    <NuxtLink to="/index">Go to Index</NuxtLink>
+    <NuxtLink to="/app/login">Go to App/Login</NuxtLink>
+</div>
+</template>
