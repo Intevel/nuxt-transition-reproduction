432d698458dabccc36180f70bce488a5294b7d8a
Conner Bachmann <github@conner-bachmann.de>
2022-11-06 17:02:01 +0100

    init: try to reproduce bug in stackblitz

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Text by Layout</h1>
+        <slot />
+    </div>
+</template>
---
+++ pages/app/login.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+    <h1>This is my Login page</h1>
+    <NuxtLink to="/index">Go to Index</NuxtLink>
+    <NuxtLink to="/about">Go to About</NuxtLink>
+</div>
+</template>
